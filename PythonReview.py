0ffc4fc356009c751dd416d44a3801633633f8f1
judit17-meet <judit.kiel@gmail.com>
2015-11-04 18:23:29 +0200

    comment

---
+++ PythonReview.py
@@ -0,0 +1,49 @@
+print("Hello, world!")
+print("hello, 'world' ")
+print ("this isn/'t a sentance!")
+nu1= 50
+nu2= 2.5
+print(nu1, nu2)
+print(nu1+nu2)
+print("this is the first number %i, this is the second number %.1f"%(nu1,nu2))
+start_list= [5,3,1,2,4]
+print(start_list[2])
+start_list.sort()
+print(start_list[2])
+a="student"
+sclass= ["student1", "student2", "student3"]
+
+print("judith")
+number1= 6
+print(number1)
+number2= 3
+print(number2)
+my_list= [11,22,33]
+for i in my_list:
+	print(i)
+import turtle
+turtle.begin_fill()
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.end_fill()
+
+
+
+
+
+
+turtle.mainloop()
+
+
+
+import turtle
+turtle.circle(100)
+turtle.main_loop
